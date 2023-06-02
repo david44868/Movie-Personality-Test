b72626d3584caee99b37a59d6e6a8ec5412a21f3
anahar1 <anahar1@asu.edu>
2023-06-02 12:56:55 -0700

    movie css

--- src/MovieRecommendations.js
+++ src/MovieRecommendations.js
@@ -1,22 +1,42 @@
-import React from 'react'
+import React from 'react';
+import './Movie.css';
 
 const MovieRecommendations = ({ recommendations }) => {
   if (!recommendations || recommendations.length === 0) {
-    return <p>No recommendations available.</p>
+    return <p>No recommendations available. Enter values again.</p>;
   }
+
+  const openIMDbLink = (url) => {
+    window.open(url, '_blank');
+  };
 
   return (
     <div>
-      <h2>Movie Recommendations</h2>
-      {recommendations.map((movie) => (
-        <div key={movie.id}>
-          <h3>{movie.title}</h3>
-          <p>{movie.overview}</p>
-          <p>Rating: {movie.vote_average}</p>
-        </div>
-      ))}
+      <h2 className="recommendations-title">Movie Recommendations</h2>
+      <div className="card-container">
+        {recommendations.map((movie) => (
+          <div
+            key={movie.id}
+            className="movie-card"
+            onClick={() => openIMDbLink(`https://www.imdb.com/title/${movie.id}/`)}
+          >
+            <h3>{movie.title}</h3>
+            <h4>IMDB Rating: {movie.rating}</h4>
+            <img src={movie.posterPath} alt={movie.title} width="200px" />
+          </div>
+        ))}
+      </div>
+      <div className="button-container">
+        <button
+          className="btn btn-primary text-lg font-bold text-white py-2 px-4 rounded"
+          style={{ backgroundColor: '#333', color: '#fff' }}
+          onClick={() => window.location.reload()}
+        >
+          Home
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieRecommendations
+export default MovieRecommendations;
--- src/PersonalityTest.js
+++ src/PersonalityTest.js
@@ -1,28 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import PersonalityTestResults from "./PersonalityTestResults";
 
 const PersonalityTest = ({ onSubmit }) => {
   const questions = [
     {
-      question: 'How comfortable are you with taking risks in life?',
+      question: "How comfortable are you with taking risks in life?",
       options: [
         {
-          value: 'risk-taker',
-          label: 'I love taking risks and embracing new challenges.',
+          value: "risk-taking",
+          label: "I love taking risks and embracing new challenges.",
         },
         {
-          value: 'cautious',
-          label: 'I prefer playing it safe and being cautious.',
+          value: "cautious",
+          label: "I prefer playing it safe and being cautious.",
         },
-        { value: 'balanced', label: 'Both, in a balanced way.' },
+        { value: "balanced", label: "Both, in a balanced way." },
       ],
     },
     {
       question:
-        'Do you find solace in nature or feel more connected to the advancements of technology?',
+        "Do you find solace in nature or feel more connected to the advancements of technology?",
       options: [
-        { value: 'nature', label: 'Nature' },
-        { value: 'technology', label: 'Technology' },
-        { value: 'balanced', label: 'Both, in a balanced way.' },
+        { value: "nature", label: "Nature" },
+        { value: "technology", label: "Technology" },
+        { value: "both nature and technology", label: "Both, in a balanced way." },
       ],
     },
     {
@@ -30,16 +31,16 @@ const PersonalityTest = ({ onSubmit }) => {
         'When it comes to adventurous activities, do you prefer engaging in them with a group of people or by yourself?',
       options: [
         {
-          value: 'group',
+          value: 'extrovert',
           label:
             'I love the thrill of group adventures and enjoy the company of others.',
         },
         {
-          value: 'solo',
+          value: 'introvert',
           label:
             'I prefer solo adventures where I can immerse myself in the experience and reflect.',
         },
-        { value: 'either', label: 'Either one is fine.' },
+        { value: 'ambivert', label: 'Either one is fine.' },
       ],
     },
     {
@@ -71,7 +72,7 @@ const PersonalityTest = ({ onSubmit }) => {
       ],
     },
     {
-      question: 'Humor: Which houseplant are you?',
+      question: 'Which houseplant are you?',
       options: [
         { value: 'family', label: 'Golden Pothos' },
         { value: 'action', label: 'Monstera' },
@@ -82,7 +83,7 @@ const PersonalityTest = ({ onSubmit }) => {
       question: 'If you had to pick a city to live in, where would it be?',
       options: [
         { value: 'action', label: 'New York City' },
-        { value: 'romcom', label: 'San Diego' },
+        { value: 'romance', label: 'San Diego' },
         { value: 'drama', label: 'Chicago' },
       ],
     },
@@ -123,46 +124,132 @@ const PersonalityTest = ({ onSubmit }) => {
       options: [
         { value: 'fantasy', label: 'A whimsical and ever-expanding library with books that come to life.' },
         { value: 'horror', label: 'A dark and mysterious labyrinth with hidden treasures and menacing creatures.' },
-        { value: 'fantasy', label: 'A serene and tranquil garden with exotic plants and talking animals.' },
+        { value: 'family', label: 'A serene and tranquil garden with exotic plants and talking animals.' },
       ],
     },
-  ]
+  ];
 
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
-  const [responses, setResponses] = useState(Array(questions.length).fill(''))
-  const [completedTest, setCompletedTest] = useState(false)
-  const [recommendations, setRecommendations] = useState([])
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [responses, setResponses] = useState(Array(questions.length).fill(""));
+  const [completedTest, setCompletedTest] = useState(false);
+  const [movieRecommendations, setMovieRecommendations] = useState(null);
+
+  // Genre IDs
+  const genreMap = {
+    "action": 28,
+    "adventure": 12,
+    "comedy": 35,
+    "drama": 18,
+    "family": 10751,
+    "fantasy": 14,
+    "horror": 27,
+    "mystery": 9648,
+    "romance": 10749,
+    "sci-fi": 878,
+    "thriller": 53,
+    "risk-taking": 53,
+    "cautious": 9648,
+    "balanced": 12,
+    "nature": 14,
+    "technology": 28,
+    "both nature and technology": 12,
+    "extrovert": 28,
+    "introvert": 14,
+    "ambivert": 12,
+  };
   
+
   const handleResponse = (question, answer) => {
     setResponses((prevResponses) => {
-      const newResponses = [...prevResponses]
-      newResponses[question] = answer
-      return newResponses
-    })
-
+      const newResponses = [...prevResponses];
+      newResponses[question] = answer;
+      // console.log("newResponses", newResponses)
+      return newResponses;
+    });
 
     setTimeout(() => {
       if (currentQuestionIndex < questions.length - 1) {
         setCurrentQuestionIndex(currentQuestionIndex + 1);
       } else {
         setCompletedTest(true);
-        onSubmit();
-      }
+        // setTimeout(() => {
+        //   setLoading(false);
+        // }, 1500);
+
+        const responseMap = new Map();
+      responses.forEach((response) => {
+        const count = responseMap.get(response) || 0;
+        responseMap.set(response, count + 1);
+      });
+
+      let mostSelectedGenre = '';
+      let secondMostSelectedGenre = '';
+      let maxCount = 0;
+      let secondMaxCount = 0;
+
+      responseMap.forEach((count, genre) => {
+        if (count > maxCount) {
+          secondMostSelectedGenre = mostSelectedGenre;
+          secondMaxCount = maxCount;
+          mostSelectedGenre = genre;
+          maxCount = count;
+        } else if (count === maxCount) {
+          secondMostSelectedGenre = genre;
+          secondMaxCount = count;
+        } else if (count > secondMaxCount) {
+          secondMostSelectedGenre = genre;
+          secondMaxCount = count;
+        }
+      });
+
+      fetchMovies(mostSelectedGenre, secondMostSelectedGenre)
+    }
     }, 350); // delay
+  };
+
+
+// Function to fetch movies
+const fetchMovies = async (mostSelectedGenre, secondMostSelectedGenre) => {
+  const apiKey = '6825b254d0d78d91c8b18db5a8c9c31a'; // Replace with your TMDb API key
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genreMap[mostSelectedGenre]},${genreMap[secondMostSelectedGenre]}`
+    );
+    const data = await response.json();
+    // Process the movie results
+    if (data.results && data.results.length > 0) {
+      const movies = data.results.slice(0, 5).map((movie) => 
+        {
+          return {
+            title: movie.title,
+            posterPath: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            overview: movie.overview,
+            rating: movie.vote_average,
+          }
+          
+        }); // Top 5 movies
+      // console.log('Movies:', movies);
+      setMovieRecommendations(movies);
+    } else {
+      // console.log('No movies found.');
+    }
+  } catch (error) {
+    console.error('Error fetching movies:', error);
   }
+};
 
   return (
-    <div className="bg-gradient-to-r from-teal-400 via-blue-500 to-purple-600 py-8 px-4 rounded-lg shadow-xl">
-
-      {currentQuestionIndex < questions.length ? (
-        <div className="max-w-lg mx-auto">
+    <div className="bg-gradient-to-r from-teal-400 via-blue-500 to-purple-600 py-8 px-4 rounded-lg shadow-xl m-10">
+      {!completedTest ? (
+        <div className="max-w-lg mx-auto p-5">
           <h3 className="text-2xl font-bold mb-6 text-white">
             {questions[currentQuestionIndex].question}
           </h3>
           {questions[currentQuestionIndex].options.map((option) => (
             <label
               key={option.value}
-              className="block py-2 px-4 rounded-md bg-white text-lg text-gray-800 mb-2 cursor-pointer hover:bg-gray-200">
+              className="block py-2 px-4 rounded-md bg-white text-lg text-gray-800 mb-2 cursor-pointer hover:bg-gray-200"
+            >
               <input
                 type="radio"
                 name={`question-${currentQuestionIndex}`}
@@ -177,14 +264,18 @@ const PersonalityTest = ({ onSubmit }) => {
             </label>
           ))}
         </div>
-      ) : (
-        <p className="text-2xl font-bold text-white">
-          Test completed. Submitting your responses...
-        </p>
+      ) : 
+      // loading ? (
+      //   <p className="text-2xl font-bold text-white">
+      //     Test completed. Submitting your responses...
+      //   </p>
+      // ) : 
+      (
+        <PersonalityTestResults responses={responses} onSubmit={onSubmit} movieRecommendations={movieRecommendations}/>
       )}
       {/* Render movie recommendations */}
     </div>
-  )
-}
+  );
+};
 
-export default PersonalityTest
+export default PersonalityTest;
